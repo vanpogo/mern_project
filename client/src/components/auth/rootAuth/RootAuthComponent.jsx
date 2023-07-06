07ee9fc7089bbbf6db68cad7355fed25dfa72735
Van Pog <jjjetbrains@mail.ru>
2023-07-06 11:19:33 +0300

    refactoring code

--- client/src/components/auth/rootAuth/RootAuthComponent.jsx
+++ client/src/components/auth/rootAuth/RootAuthComponent.jsx
@@ -1,60 +1,78 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import styles from "./style.module.css";
-import {useLocation, useNavigate} from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Register from "../register/Register.jsx";
 import Login from "../login/Login.jsx";
-import {Box} from "@mui/material";
-import {useDispatch, useSelector} from "react-redux"
-import { isCheckAuth, loginUser, registerUser } from '../../../redux/slices/auth/authSlice';
+import { Box } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  isCheckAuth,
+  loginUser,
+  registerUser,
+} from "../../../redux/slices/auth/authSlice";
 
 function RootAuthComponent() {
-    const {pathname} = useLocation();
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const isAuth = useSelector(isCheckAuth);
-    const {token} = useSelector(state => state.auth)
+  const isAuth = useSelector(isCheckAuth);
+  const { token } = useSelector((state) => state.auth);
 
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const [firstName,setFirstName] = useState("");
-    const [userName,setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [userName, setUserName] = useState("");
 
-    const handleSubmit = async(e)=> {
-        e.preventDefault();
-       
-        if(pathname === "/register"){
-            const userData = {email,password,firstName,userName}
-            dispatch(registerUser(userData));
-        }else{
-            const userData = {email,password};
-            dispatch(loginUser(userData))
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (pathname === "/register") {
+        const userData = { email, password, firstName, userName };
+        dispatch(registerUser(userData));
+      } else {
+        const userData = { email, password };
+        dispatch(loginUser(userData));
+      }
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    useEffect(() => {
-        if(isAuth){
-            window.localStorage.setItem("token",token)
-            navigate("/home")
-        }
-    },[token])
+  useEffect(() => {
+    if (isAuth) {
+      window.localStorage.setItem("token", token);
+      navigate("/home");
+    }
+  }, [token]);
   return (
-    <div className= {styles.root}>
-        <Box sx = {{
-            maxWidth : "650px",
-            width: "calc(100% / 3)",
-            margin : "auto",
-            borderRadius : "5px",
-            padding : "5px",
-            boxShadow : "5px 5px 10px #000"
-        }}>
-            <form onSubmit={handleSubmit}>
-                {pathname === "/register" ? <Register setEmail={setEmail} setFirstName={setFirstName} setUserName={setUserName} setPassword={setPassword}/> 
-                : pathname === "/login" ? <Login setEmail={setEmail} setPassword={setPassword}/> : null}
-            </form>
-        </Box>
+    <div className={styles.root}>
+      <Box
+        sx={{
+          maxWidth: "650px",
+          width: "calc(100% / 3)",
+          margin: "auto",
+          borderRadius: "5px",
+          padding: "5px",
+          boxShadow: "5px 5px 10px #000",
+        }}
+      >
+        <form onSubmit={handleSubmit}>
+          {pathname === "/register" ? (
+            <Register
+              setEmail={setEmail}
+              setFirstName={setFirstName}
+              setUserName={setUserName}
+              setPassword={setPassword}
+            />
+          ) : pathname === "/login" ? (
+            <Login setEmail={setEmail} setPassword={setPassword} />
+          ) : null}
+        </form>
+      </Box>
     </div>
-  )
+  );
 }
 
-export default RootAuthComponent
+export default RootAuthComponent;
